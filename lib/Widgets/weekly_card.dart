ff6c84d30700ec78e68541ae53b614c0a3ea7f53
devpark435 <devpark435@gmail.com>
2023-09-22 17:38:22 +0900

    feat #1 : weekly card rank font

--- lib/Widgets/weekly_card.dart
+++ lib/Widgets/weekly_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Widget weeklyCard(size, context) {
+Widget weeklyCard(size, context, index) {
   return Padding(
     padding: const EdgeInsets.symmetric(horizontal: 20.0),
     child: Card(
@@ -9,69 +9,86 @@ Widget weeklyCard(size, context) {
       child: SizedBox(
         width: size,
         height: size,
-        child: Column(children: [
-          Expanded(
-              flex: 2,
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25))),
-              )),
-          Expanded(
-              flex: 1,
-              child: Container(
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(25),
-                          bottomRight: Radius.circular(25))),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: ListTile(
-                          title: Text(
-                            "테스트 이름",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleMedium!
-                                .apply(fontWeightDelta: 2),
+        child: Stack(
+          children: [
+            Column(children: [
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25))),
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(25),
+                              bottomRight: Radius.circular(25))),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: ListTile(
+                              title: Text(
+                                "테스트 이름",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .apply(fontWeightDelta: 2),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Padding(
-                                padding: const EdgeInsets.only(left: 20.0),
-                                child: InkWell(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        border: Border.all(
-                                            color: Colors.blue.shade300,
-                                            width: 2)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 3.0, horizontal: 5),
-                                      child: Text(
-                                        "심리테스트",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .labelSmall!
-                                            .apply(color: Colors.blue.shade300),
+                          Expanded(
+                            flex: 1,
+                            child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Padding(
+                                    padding: const EdgeInsets.only(left: 20.0),
+                                    child: InkWell(
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            border: Border.all(
+                                                color: Colors.blue.shade300,
+                                                width: 2)),
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 3.0, horizontal: 5),
+                                          child: Text(
+                                            "심리테스트",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .labelSmall!
+                                                .apply(
+                                                    color:
+                                                        Colors.blue.shade300),
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                  onTap: () {},
-                                ))),
-                      )
-                    ],
-                  )))
-        ]),
+                                      onTap: () {},
+                                    ))),
+                          )
+                        ],
+                      )))
+            ]),
+            Positioned(
+                top: size / 2,
+                left: 10,
+                child: Text(
+                  "${index + 1}",
+                  style: Theme.of(context).textTheme.displayMedium!.apply(
+                      color: Colors.grey.shade300,
+                      fontStyle: FontStyle.italic,
+                      fontWeightDelta: 3),
+                ))
+          ],
+        ),
       ),
     ),
   );
