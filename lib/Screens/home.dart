5fb07921343e8fab176ce5efaf4cc0c73c957e33
devpark435 <devpark435@gmail.com>
2023-09-23 19:50:55 +0900

    feat #1 : 검색기능 추가

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,12 +1,21 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:psychologicaltest_flutter_app/Screens/entry_test.dart';
+import 'package:psychologicaltest_flutter_app/Screens/psychological_test.dart';
+import 'package:psychologicaltest_flutter_app/Widgets/search.dart';
 import 'package:psychologicaltest_flutter_app/Widgets/testList_card.dart';
 import 'package:psychologicaltest_flutter_app/Widgets/weekly_card.dart';
+import 'package:psychologicaltest_flutter_app/model/PsychologicalTest_model.dart';
+import 'package:psychologicaltest_flutter_app/provider/Quiz_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var quizProvider = Provider.of<QuizProvider>(context, listen: false);
+    final size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
           centerTitle: false,
@@ -19,7 +28,11 @@ class HomeScreen extends StatelessWidget {
           ),
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showSearch(
+                    context: context,
+                    delegate: QuizSearchDelegate(quizProvider.quizzes));
+              },
               icon: const Icon(
                 Icons.search,
                 size: 32,
@@ -33,57 +46,99 @@ class HomeScreen extends StatelessWidget {
                 ))
           ],
         ),
-        body: SafeArea(
-            child: Column(
-          children: [
-            Expanded(
-                flex: 1,
-                child: Stack(
-                  alignment: Alignment.centerLeft,
+        body: FutureBuilder(
+            future: quizProvider.loadQuizzes(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return const Center(child: Text('Error'));
+              } else {
+                return SafeArea(
+                    child: Column(
                   children: [
-                    Column(
-                      children: [
-                        Expanded(
-                            flex: 2,
-                            child: Container(
-                              color: Colors.blue.shade300,
-                            )),
-                        Expanded(
-                            flex: 1,
-                            child: Container(
-                              color: Colors.transparent,
-                            ))
-                      ],
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.25,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 5,
-                        itemBuilder: (BuildContext context, int index) {
-                          var size = MediaQuery.of(context).size.height * 0.25;
-                          return GestureDetector(
-                            onTap: () {
-                              Navigator.pushNamed(context, '/entry');
+                    Expanded(
+                        flex: 1,
+                        child: Stack(
+                          alignment: Alignment.centerLeft,
+                          children: [
+                            Column(
+                              children: [
+                                Expanded(
+                                    flex: 2,
+                                    child: Container(
+                                      color: Colors.blue.shade300,
+                                    )),
+                                Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      color: Colors.transparent,
+                                    ))
+                              ],
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.25,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: quizProvider.quizzes.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  var size =
+                                      MediaQuery.of(context).size.height * 0.25;
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EntryTestScreen(
+                                                    quizData: quizProvider
+                                                        .quizzes[index],
+                                                  )));
+                                    },
+                                    child: weeklyCard(size, context, index),
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: ScrollConfiguration(
+                          behavior: MyCustomScrollBehavior(),
+                          child: ListView.separated(
+                            scrollDirection: size.width > 400
+                                ? Axis.horizontal
+                                : Axis.vertical,
+                            itemCount: 6,
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                            itemBuilder: (BuildContext context, int index) {
+                              return SizedBox(
+                                  width: 300,
+                                  // size.width > 400 ? 400 : size.width * .9,
+                                  height: 300,
+                                  // size.height > 900
+                                  //     ? 400
+                                  //     : size.height * .3,
+                                  child: testListCard(context, size.width));
                             },
-                            child: weeklyCard(size, context, index),
-                          );
-                        },
-                      ),
-                    )
+                          ),
+                        ))
                   ],
-                )),
-            Expanded(
-                flex: 1,
-                child: ListView.separated(
-                  itemCount: 5,
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
-                  itemBuilder: (BuildContext context, int index) {
-                    return testListCard(context);
-                  },
-                ))
-          ],
-        )));
+                ));
+              }
+            }));
   }
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
 }
---
+++ lib/Widgets/search.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:psychologicaltest_flutter_app/Screens/entry_test.dart';
+import 'package:psychologicaltest_flutter_app/model/PsychologicalTest_model.dart';
+
+class QuizSearchDelegate extends SearchDelegate<PsychologicalTestModel> {
+  final List<PsychologicalTestModel> quizzes;
+
+  QuizSearchDelegate(this.quizzes);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        // close(context,null);
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final filteredQuizzes = quizzes
+        .where((quiz) => quiz.title.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+        itemCount: filteredQuizzes.length,
+        itemBuilder: (context, index) => ListTile(
+              title: Text(filteredQuizzes[index].title),
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          EntryTestScreen(quizData: filteredQuizzes[index]))),
+            ));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = quizzes
+        .where(
+            (quiz) => quiz.title.toLowerCase().startsWith(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+        itemCount: suggestionList.length,
+        itemBuilder: (context, index) =>
+            ListTile(title: Text(suggestionList[index].title), onTap: () {}));
+  }
+}
