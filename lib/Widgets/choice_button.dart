3c4a6d859f11acf004425a9a9ceaf1197f061d84
devpark435 <devpark435@gmail.com>
2023-10-04 15:44:54 +0900

    feat #6 : choice button mouse hover animation

--- lib/Widgets/choice_button.dart
+++ lib/Widgets/choice_button.dart
@@ -13,47 +13,65 @@ class ChoiceButton extends StatefulWidget {
 
 class _ChoiceButtonState extends State<ChoiceButton> {
   bool isTapped = false;
+  double size = 50;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (details) {
-        setState(() {
-          isTapped = true;
-        });
-      },
-      onTapUp: (details) async {
-        await Future.delayed(const Duration(milliseconds: 500));
-        setState(() {
-          isTapped = false;
-        });
-        widget.onTap();
-      },
-      child: InkWell(
-          customBorder: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
-          ),
-          child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              width: MediaQuery.of(context).size.width *
-                  (isTapped ? .8 : .7), // size increases when tapped
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: isTapped
-                    ? Theme.of(context).colorScheme.primaryContainer
-                    : Theme.of(context).colorScheme.primaryContainer,
-              ),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(widget.answerText,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium!
-                          .apply(fontWeightDelta: 3)),
+    return MouseRegion(
+      onEnter: (PointerEvent details) => _increaseSize(),
+      onExit: (PointerEvent details) => _resetSize(),
+      child: GestureDetector(
+        onTapDown: (details) {
+          setState(() {
+            isTapped = true;
+          });
+        },
+        onTapUp: (details) async {
+          await Future.delayed(const Duration(milliseconds: 500));
+          setState(() {
+            isTapped = false;
+          });
+          widget.onTap();
+        },
+        child: InkWell(
+            customBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                width: MediaQuery.of(context).size.width *
+                    (isTapped ? .8 : .7), // size increases when tapped
+                height: size,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: isTapped
+                      ? Theme.of(context).colorScheme.primaryContainer
+                      : Theme.of(context).colorScheme.primaryContainer,
                 ),
-              ))),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(widget.answerText,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .apply(fontWeightDelta: 3)),
+                  ),
+                ))),
+      ),
     );
+  }
+
+  void _increaseSize() {
+    setState(() {
+      size = 70.0;
+    });
+  }
+
+  void _resetSize() {
+    setState(() {
+      size = 50.0;
+    });
   }
 }
