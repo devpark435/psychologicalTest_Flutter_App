3d2e6bc620b60d88ddab8e2b9b5c6c70eafcf179
devpark435 <devpark435@gmail.com>
2023-10-06 20:25:23 +0900

    feat #4 : result json data model

--- lib/model/Result_model.dart
+++ lib/model/Result_model.dart
@@ -1,10 +1,14 @@
 class Result {
   final String title;
   final String content;
+  final String imagePath;
 
-  Result({required this.title, required this.content});
+  Result({required this.title, required this.content, required this.imagePath});
 
   factory Result.fromJson(Map<String, dynamic> json) {
-    return Result(title: json["title"], content: json["content"]);
+    return Result(
+        title: json["title"],
+        content: json["content"],
+        imagePath: json["imagePath"]);
   }
 }
